b3cd3c49e85666eaf939e4a3cf25bdfca60290c6
soulmachine <soulmachine@gmail.com>
2020-02-10 17:29:14 -0800

    Changed arguments to positional arguments

--- src/cli.ts
+++ src/cli.ts
@@ -1,26 +1,28 @@
 #!/usr/bin/env node
 /* eslint-disable no-console */
-import yargs from 'yargs';
 import { EXCHANGES } from 'crypto-crawler/dist/crawler';
-
+import yargs from 'yargs';
 import crawl from './index';
 
-const { argv } = yargs.options({
-  exchange: {
-    choices: EXCHANGES,
-    type: 'string',
-    demandOption: true,
-    default: 'Newdex',
-  },
-  pair: {
-    type: 'string',
-    demandOption: true,
-    default: 'EIDOS_EOS',
-  },
-});
+const { argv } = yargs
+  // eslint-disable-next-line no-shadow
+  .command('$0 <exchange> <pair>', 'Get realtime BBO', yargs => {
+    yargs
+      .positional('exchange', {
+        choices: EXCHANGES,
+        type: 'string',
+        default: 'Coinbase',
+        describe: 'The exchange name',
+      })
+      .positional('pair', {
+        type: 'string',
+        describe: 'The trading pair',
+        default: 'BTC_USD',
+      });
+  });
 
 const { exchange, pair } = argv;
 
 (async () => {
-  await crawl(exchange, [pair]);
+  await crawl(exchange as string, [pair as string]);
 })();
